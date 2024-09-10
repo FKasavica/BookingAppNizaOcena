ae9e37d8b5f01849e5f139d68a2d195cfc0d445e
FKasavica <filip.kasavica@gmail.com>
2024-09-10 18:53:56 +0200

    feat/view_base_+_registration
    UserView
    RegistrationView
    RelayCommand
    ViewModels

---
+++ BookingAppNizaOcena/ViewModels/UserViewModel.cs
@@ -0,0 +1,66 @@
+﻿using BookingAppNizaOcena.Applications.Services;
+using BookingAppNizaOcena.Domain.Models;
+using BookingAppNizaOcena.Utilities;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace BookingAppNizaOcena.ViewModels
+{
+    public class UserViewModel : BaseViewModel
+    {
+        private readonly UserService _userService;
+
+        public ObservableCollection<User> Users { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RegisterCommand { get; }
+
+        public UserViewModel(UserService userService)
+        {
+            _userService = userService;
+            Users = new ObservableCollection<User>(_userService.GetAllUsers());
+
+            LoginCommand = new RelayCommand(Login);
+            RegisterCommand = new RelayCommand(Register);
+        }
+
+        private void Login()
+        {
+            var user = _userService.Login(Email, Password);
+            if (user != null)
+            {
+                ErrorMessage = string.Empty;
+            }
+            else
+            {
+                ErrorMessage = "Invalid email or password!";
+            }
+            OnPropertyChanged(nameof(ErrorMessage));
+        }
+
+        private void Register()
+        {
+            var existingUser = _userService.GetAllUsers().FirstOrDefault(u => u.Email == Email);
+            if (existingUser != null)
+            {
+                ErrorMessage = "User with this email already exists!";
+            }
+            else
+            {
+                var newUser = new User
+                {
+                    Email = Email,
+                    Password = Password,
+                };
+                _userService.Register(newUser);
+                Users.Add(newUser);
+                ErrorMessage = "Registration successful!";
+            }
+            OnPropertyChanged(nameof(ErrorMessage));
+        }
+    }
+}
---
+++ BookingAppNizaOcena/Views/UserView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using BookingAppNizaOcena.Applications.Services;
+using BookingAppNizaOcena.Repository;
+using BookingAppNizaOcena.ViewModels;
+using System.Windows;
+
+namespace BookingAppNizaOcena.Views
+{
+    public partial class UserView : Window
+    {
+        public UserView()
+        {
+            InitializeComponent();
+            DataContext = new UserViewModel(new UserService(new UserRepository()));
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext != null)
+            {
+                ((UserViewModel)DataContext).Password = PasswordBox.Password;
+            }
+        }
+    }
+}
